c78c287595e59a1a02b43d6b253cd882877235bd
Jordon Smith <jsmith72@uoguelph.ca>
2018-12-13 16:53:44 -0500

    Added style to home screen with stubbed links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,27 @@
 @section('content')
 
     <div class="content">
-        <div class="m-b-md">
-            <img src="{{asset('img/donateABLE-square-colour-500.png')}}" style="width:400px;height: auto;"/>
-        </div>
+        <div class="site-callout">
+                <img src="{{asset('img/donateABLE-square-colour-500.png')}}" style="width:400px;height: auto;"/>
 
-        <div class="m-b-md">
-            <h1>Coming Soon</h1>
-            <h2>the newest and easiest way to donate</h2>
+                <h1>Support Local Charities in Guelph</h1>
+                <h2>Without spending a dime</h2>
+                <p style="padding-bottom: 5px;">Simply lend your computer's power to generate real value for the charity of your choice.</br>
+                Donate to your favourite charities without opening your wallet.</br>
+                It is that easy.</p>
+        </div>
+        <div class="site-links">
+            <div class="left-link">
+                <a href="#" class="link-box">Select Your Charity</a>
+                <a href="#" class="link-box">How It Works</a>
+                <a href="#" class="link-box">Privacy Policy</a>
+            </div>
+            <div class="right-link">
+                <a class="stat-box">Currently Donating 1,234</a>
+                <a class="stat-box">Donators to date 12,345</a>
+                <a class="stat-box">Total Hashes 12,345,678</a>
+            </div>
         </div>
       </div>
-
-    </div>
 
 @endsection
