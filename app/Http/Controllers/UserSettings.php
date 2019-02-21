3c524435eb6d5de0085ea8f95d9eb094e2bcaae7
Jordon Smith <jsmith72@uoguelph.ca>
2019-02-21 16:54:12 -0500

    Fix build files

--- app/Http/Controllers/UserSettings.php
+++ app/Http/Controllers/UserSettings.php
@@ -13,6 +13,7 @@ class UserSettings extends Controller
     public function __invoke(Request $request) {
 
         $user = Auth::user();
+<<<<<<< HEAD
         if (Hash::check($request->input('password'), $user->password)) {
             $data = array(
                 'firstName' => $request->input('fname', $user->firstName),
@@ -42,6 +43,35 @@ class UserSettings extends Controller
             }
             $user->update($data);
             return view('home', array('user' => Auth::user()));
+=======
+
+           $validator = Validator::make($request->all(),[
+             'email' => ['required', 'email', Rule::unique('users')->ignore($user->id)],
+             'username' => [Rule::unique('users')->ignore($user->id), nullable],
+
+         ]);
+
+         if ($validator->fails()) {
+             return Redirect::back()->withErrors($validator)->withInput();
+        }
+
+
+        $data = array(
+            'firstName' => $request->input('fname', $user->firstName),
+            'lastName' => $request->input('lname', $user->lastName),
+            'username' => $request->input('username', $user->username),
+            'email' => $request->input('email', $user->email),
+            'communicationOptIn' => $user->communicationOptIn,
+            'publishStatsOptIn' => $user->publishStatsOptIn,
+        );
+        if ($request->has('communicationOptIn')) {
+            $data['communicationOptIn'] = true;
+        } else {
+            $data['communicationOptIn'] = false;
+        }
+        if ($request->has('publishStatsOptIn')) {
+            $data['publishStatsOptIn'] = true;
+>>>>>>> f85af1e... Filename cleanup and fix username validation
         } else {
             return Redirect::to('/home')->withError('Your password is incorrect. Please enter your current password to make changes to your account.');
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,9 @@ mix.react('resources/assets/js/app.js', 'public/js')
     .babel('resources/assets/js/donate.js', 'public/js/donate.js')
     .babel('resources/assets/js/contactUs.js', 'public/js/contactUs.js')
     .babel('resources/assets/js/singlepage.js', 'public/js/singlepage.js')
+<<<<<<< HEAD
+=======
+    .babel('resources/assets/js/profilePage.js', 'public/js/profilepage.js')
+>>>>>>> f85af1e... Filename cleanup and fix username validation
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/tabbedNavigation.scss', 'public/css');
