50c9a35343692bcf74b9124af6de932448df3aed
Jordon Smith <jsmith72@mail.uoguelph.ca>
2019-03-07 10:23:01 -0500

    Changed header font to Barlow (#51)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 
     <!-- Styles -->
