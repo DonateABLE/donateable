b125e61892a68fe5aaf9caf6798cd03b991e0cb3
Luke Pritchard <luke.pritchard@protonmail.com>
2019-04-30 09:22:09 -0400

    Added comment field to seed as per new schema requirements.

--- database/seeds/GHSCharitySeeder.php
+++ database/seeds/GHSCharitySeeder.php
@@ -61,21 +61,21 @@ The Guelph Humane Society is contracted by the City of Guelph, the Township of C
                 'faLink' => 'fas fa-bone',
                 'title' => 'Bowl of Kibble',
                 'description' => 'Cost = $.50 - Approx. 500,000 Hashes',
-
+                'comment' => ''
             ],
             [
                 'charityId' => $charityId,
                 'faLink' => 'fas fa-shopping-bag',
                 'title' => 'Bag of Food',
                 'description' => 'Cost = $40 - Approx. 4,000,000,000 Hashes',
-
+                'comment' => ''
             ],
             [
                 'charityId' => $charityId,
                 'faLink' => 'fas fa-microchip',
                 'title' => 'Micro Chip Scanner',
                 'description' => 'Cost = $80 - Approx. 8,000,000,000 Hashes',
-
+                'comment' => ''
             ],
         ]);
 
