eeef44afdaf2a5a4ebda0db8d0f791fc603e57e5
Jordon Smith <jsmith72@mail.uoguelph.ca>
2019-05-03 16:22:29 -0700

    Integrated React project

---
+++ resources/assets/js/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+/* An example React component */
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
+
+/* The if statement is required so as to Render the component on pages that have a div with an ID of "root";
+*/
+
+if (document.getElementById("mobile")) {
+  ReactDOM.render(<Main />, document.getElementById("mobile"));
+}
