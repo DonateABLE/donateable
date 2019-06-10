9771e19eb3711e464022b59f774c7b989a265f32
Luke Pritchard <luke.pritchard@protonmail.com>
2019-06-10 15:17:28 -0400

    Added Components To Be Integrated

---
+++ resources/assets/js/components/integration/Image.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from './img/donateABLE-square-colour-500.png'
+import '/Users/administrator/dev/donate-mobile/node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function Image () {
+  // Returning the logo to the top level file
+  return <img src={logo} class='img-fluid' alt='Logo' />
+}
+
+export default Image
