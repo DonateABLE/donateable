fce31ad8e2c497eb1ea684034e05a58193d658db
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-03 15:23:44 -0400

    Added Comments

--- resources/assets/js/components/Footer.js
+++ resources/assets/js/components/Footer.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 function Footer () {
   return (
     <div style={{
-              color: 'white', 
-              display: 'flex', 
-              justifyContent: 'center',
-              marginTop: 10,
-              marginBottom: 10 }}>
+      color: 'white', 
+      display: 'flex', 
+      justifyContent: 'center',
+      marginTop: 10,
+      marginBottom: 10 }}>
               Powered By &nbsp;
       <a href="https://www.synergenics.ca" target="_blank">
         <img src="/img/logo/Synergenics-Logo-2008-White.png" style={{width: 120}}/>
