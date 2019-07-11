d23c04e989271ee64fce5573ae46b8960bf9ac60
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-11 11:16:57 -0400

    Fix: More Contact Modal Content

--- resources/assets/js/components/Navigation/ButtonGrouping.js
+++ resources/assets/js/components/Navigation/ButtonGrouping.js
@@ -134,7 +134,7 @@ class ButtonGrouping extends Component {
             <Modal.Title id="example-custom-modal-styling-title" className='text-center'
              style={{color: '#FFFFFF', paddingLeft:'17%'}}
              >
-             <img src='/img/logo/donateABLE-long-white-500.png' style={{width: '70%'}}/>
+     
             </Modal.Title>
           </Modal.Header>
 
@@ -146,6 +146,8 @@ class ButtonGrouping extends Component {
             overflowY: 'auto',
             paddingBottom:'5%'}}>
           
+          <img src='/img/logo/donateABLE-long-white-500.png' style={{width: '70%'}}/>
+
           <ModalText 
             fontSize1={14} 
             fontSize2={14} 
@@ -203,6 +205,8 @@ class ButtonGrouping extends Component {
           </Modal.Body>
   
         </Modal>
+
+        {/* HOW IT WORKS MODAL*/}
         
         <Modal 
           size='lg'
@@ -218,10 +222,8 @@ class ButtonGrouping extends Component {
             <Modal.Title id="example-custom-modal-styling-title" className='text-center'
              style={{color: '#FFFFFF', paddingLeft:'30px'}}
              >
-             <img src='/img/logo/donateABLE-long-white-500.png' style={{width: '70%'}}/>
             </Modal.Title>
           </Modal.Header>
-
 
           <Modal.Body style={{
             display: 'flex', 
@@ -229,6 +231,7 @@ class ButtonGrouping extends Component {
             alignItems: 'center', 
             paddingTop: '0px',
             paddingBottom:'10px'}}>
+            <img src='/img/logo/donateABLE-long-white-500.png' style={{width: '70%'}}/>
           <ModalText fontSize1={14} fontSize2={14} text1='USE YOUR COMPUTER POWER TO GENERATE' text2='MONEY FOR YOUR CHOSEN CHARITY'/>
           
           <hr width={'69%'} noshade  style={{display: 'block', height: '1px',
@@ -323,24 +326,37 @@ class ButtonGrouping extends Component {
             paddingTop: '0px',
             paddingBottom:'0px'}}>
           <img src='/img/logo/donateABLE-long-white-500.png' style={{width: '70%'}}/>
-          <ModalText fontSize1={20} fontSize2={14} text1='THANK YOU FOR VISITING' text2='WE WOULD LIKE TO HEAR FROM YOU' />
+          <ModalText style={{marginBottom: '10%'}} fontSize1={20} fontSize2={14} text1='THANK YOU FOR VISITING' text2='WE WOULD LIKE TO HEAR FROM YOU' />
+          
+          <hr width={'69%'} noshade  style={{display: 'block', height: '1px',
+              border: 0, borderTop: '1px solid #FFFFFF',
+              marginTop: '4%', padding: 0}}/>
 
-          </Modal.Body>
-
-          <Modal.Body> 
             {/* JOIN THE PROGRAM */}
+            <img src='/img/contactus/Contact-Us-Join-the-Program.png' style={{width: '80%'}}/>
+
             <p style={{color: '#FFFFFF'}}>
           Are you a local, Guelph and Wellington County, non-profit charity that would like 
           to be featured on donateABLE? Simply fill out the form below and a member of our 
           team will be in touch with the next steps.
-          </p>  
+          </p>
+
+          <hr width={'69%'} noshade  style={{display: 'block', height: '1px',
+              border: 0, borderTop: '1px solid #FFFFFF',
+              marginTop: '4%', padding: 0}}/>
 
           {/* Technical Support */}
+          <img src='/img/contactus/Contact-Us-Technical-Support.png' style={{width: '80%'}}/>
           <p style={{color: '#FFFFFF'}}>
           If you are having any problems with the site functionality or have a technical 
           question please fill out the form below to submit a ticket. A member of our team 
           will get back to you within 48 business hours.
           </p>
+          <hr width={'69%'} noshade  style={{display: 'block', height: '1px',
+              border: 0, borderTop: '1px solid #FFFFFF',
+              marginTop: '4%', padding: 0}}/>
+
+          <WhiteButton Show={this.handleShowFAQ} Hide={this.handleHideContact} buttonText='VIEW FREQUENTLY ASKED QUESTIONS' />
           </Modal.Body>
         </Modal>
 
