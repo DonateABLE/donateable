1d6ec8944482f6b12bea41da2b412cd90f4490a4
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-19 13:44:58 -0400

    Fix: Forgot to import react AHH

--- resources/assets/js/components/Navigation/Modals/Tour/Tour.js
+++ resources/assets/js/components/Navigation/Modals/Tour/Tour.js
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { Carousel } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Carousel, Container } from 'react-bootstrap'
 
 class Tour extends Component {
 
@@ -8,35 +8,39 @@ class Tour extends Component {
       <div>
         <Carousel>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/logo/D-Coloured-250x250.png"
+                alt="First slide"
+              />
+            </Container>
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Third slide"
-            />
-
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/logo/D-Coloured-250x250.png"
+                alt="Third slide"
+              />
+            </Container>
             <Carousel.Caption>
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/logo/D-Coloured-250x250.png"
+                alt="Third slide"
+              />
+            </Container>
             <Carousel.Caption>
               <h3>Third slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
