6c4cfbfdd471cd1c667170ddebdde8284d4f294b
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-19 16:32:58 -0400

    Feature: Seven Slides added to Take a Tour

--- resources/assets/js/components/Navigation/Modals/Tour/Tour.js
+++ resources/assets/js/components/Navigation/Modals/Tour/Tour.js
@@ -11,7 +11,7 @@ class Tour extends Component {
             <Container>
               <img
                 className="d-block img-fluid"
-                src="/img/logo/D-Coloured-250x250.png"
+                src="/img/tour/tour1.png"
                 alt="First slide"
               />
             </Container>
@@ -20,12 +20,13 @@ class Tour extends Component {
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
+          
           <Carousel.Item>
             <Container>
               <img
                 className="d-block img-fluid"
-                src="/img/logo/D-Coloured-250x250.png"
-                alt="Third slide"
+                src="/img/tour/tour2.png"
+                alt="Second slide"
               />
             </Container>
             <Carousel.Caption style={{marginTop: '10%'}}>
@@ -33,16 +34,72 @@ class Tour extends Component {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
+          
           <Carousel.Item>
             <Container>
               <img
                 className="d-block img-fluid"
-                src="/img/logo/D-Coloured-250x250.png"
-                alt="Third slide"
+                src="/img/tour/tour3.png"
+                alt="Three slide"
               />
             </Container>
             <Carousel.Caption style={{marginTop: '10%'}}>
               <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/tour/tour4.png"
+                alt="Fourth slide"
+              />
+            </Container>
+            <Carousel.Caption style={{marginTop: '10%'}}>
+              <h3>Fourth slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/tour/tour5.png"
+                alt="Fifth slide"
+              />
+            </Container>
+            <Carousel.Caption style={{marginTop: '10%'}}>
+              <h3>Fifth slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/tour/tour6.png"
+                alt="Sixth slide"
+              />
+            </Container>
+            <Carousel.Caption style={{marginTop: '10%'}}>
+              <h3>Sixth slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          
+          <Carousel.Item>
+            <Container>
+              <img
+                className="d-block img-fluid"
+                src="/img/tour/tour7.png"
+                alt="Seventh slide"
+              />
+            </Container>
+            <Carousel.Caption style={{marginTop: '10%'}}>
+              <h3>Seventh slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
             </Carousel.Caption>
           </Carousel.Item>
