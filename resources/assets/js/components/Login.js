3f7f60820cc7e6bed1f665842ce566e8b2018b7a
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-24 17:16:50 -0400

    Feature: Skeleton for Login Page

--- resources/assets/js/components/Login.js
+++ resources/assets/js/components/Login.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import Text from './Text'
-import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'
+import LoginForm from './Login/LoginForm'
 
 class Login extends Component {
   render () {
     return (
-      <div>
-        <Container style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
+      <div className='text-center'>
+        <img src='/img/logo/donateABLE-long-colour-500.png' style={{width: '60%'}} />
+        <br />
 
-          <img src='/img/logo/donateABLE-long-colour-250.png' style={{width: '100%'}} />
-          <br />
-          <p style={{color: '#26607D', fontWeight: '700'}}>
+        <p style={{color: '#26607D', fontWeight: '700', width: '80%', margin: 'auto'}}>
           Simply lend your computer’s power to generate
           real value for the charity of your choice. Donate
           to your favourite charities without opening your
           wallet. It is that easy!
-          </p>
-        </Container>
+        </p>
+
+        {/* Divider line between the Components */}
+        <hr width={'69%'} noshade  style={{display: 'block', height: '1px',
+          border: 0, borderTop: '1px solid #26607D',
+          marginTop: '4%', padding: 0 } }/>
+
+        <h1 style={{margin: 'auto', width: '70%'}}> LOGIN</h1>
       </div>
     )
   }
