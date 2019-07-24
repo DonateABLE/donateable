56cdf06602fa7dd86ff692fa42287ad951196775
Luke Pritchard <luke.pritchard@protonmail.com>
2019-07-24 15:39:59 -0400

    Feature: Content for Login Screen

--- resources/assets/js/components/Login.js
+++ resources/assets/js/components/Login.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 import Text from './Text'
+import { Container } from 'react-bootstrap';
 
 class Login extends Component {
   render () {
     return (
-      <div className='text-center'>
-        <h1> Login Screen </h1>
+      <div>
+        <Container style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+
+          <img src='/img/logo/donateABLE-long-colour-250.png' style={{width: '100%'}} />
+          <br />
+          <p style={{color: '#26607D', fontWeight: '700'}}>
+          Simply lend your computer’s power to generate
+          real value for the charity of your choice. Donate
+          to your favourite charities without opening your
+          wallet. It is that easy!
+          </p>
+        </Container>
       </div>
     )
   }
