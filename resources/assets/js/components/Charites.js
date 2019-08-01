76c30ad1aa3b24e170189f6c903a2fd9b041deea
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-01 10:33:28 -0400

    New Charity Cards

--- resources/assets/js/components/Charites.js
+++ resources/assets/js/components/Charites.js
@@ -1,11 +1,16 @@
 import React, { Component }from 'react'
 import CharityCards from './Charities/CharityCards'
+import { Container } from 'react-bootstrap';
 
 class Charities extends Component {
   render () {
     return(
       <div>
-        <CharityCards /> 
+        <Container style={{background: 'white'}}>
+          <CharityCards charityName='DONATEABLE' image='/img/charity/donateable-charity.png' />
+          <CharityCards charityName='VICTIM SERVICES WELLINGTON' image='/img/charity/Victim-Services.png' />
+          <CharityCards charityName='COMING SOON' />
+        </Container>
       </div>
     )
   }
