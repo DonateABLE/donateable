42310227e60491b5ae614c7a9ed575fa0d911e7f
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-12 11:27:27 -0400

    Fix: Grid Styling for Image and Text

--- resources/assets/js/components/Charities/Charity Content/CharityTemplate.js
+++ resources/assets/js/components/Charities/Charity Content/CharityTemplate.js
@@ -36,28 +36,48 @@ const CharityTemplate = props => {
                 </NavLink>
             </Container>
             {/* BE SURE TO CHECK VW UNITS IN FONT SIZE */}
+
+            <Container
+                style={{
+                    backgroundColor: "#45A6D7",
+                    color: "white",
+                    display: "grid | inline-grid",
+                    gridAutoFlow: "row dense"
+                }}
+            >
+                <div style={{display: 'grid'}}>
+                    <img
+                        src="/img/charity/Victim-Services.png"
+                        style={{ width: "100px" }}
+                    />
+                </div>
+
+                <div style={{display: 'grid'}}>Placeholder text</div>
+            </Container>
+
+            {/* SOCIAL MEDIA BUTTON CONTAINER */}
             <Container
                 className="text-center"
-                style={{ backgroundColor: "#45A6D7", fontSize: "5vw" }}
+                style={{ backgroundColor: "#26607D", fontSize: "5vw" }}
             >
                 <a href="https://www.facebook.com/www.vswguelph.on.ca/">
                     <FontAwesomeIcon
                         icon={faFacebookF}
-                        style={{ color: "white"}}
+                        style={{ color: "white" }}
                     />
                 </a>
                 &nbsp; &nbsp;
                 <a href="https://www.facebook.com/www.vswguelph.on.ca/">
                     <FontAwesomeIcon
                         icon={faTwitter}
-                        style={{ color: "white"}}
+                        style={{ color: "white" }}
                     />
                 </a>
                 &nbsp; &nbsp;
                 <a href="https://www.facebook.com/www.vswguelph.on.ca/">
                     <FontAwesomeIcon
                         icon={faGlobe}
-                        style={{ color: "white"}
+                        style={{ color: "white" }}
                     />
                 </a>
             </Container>
