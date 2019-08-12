12d404d4de35e40d7d03d87c869c57b6f46efd7a
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-12 12:48:03 -0400

    Fix: Stylings for the text in charities

--- resources/assets/js/components/Charities/Charity Content/CharityTemplate.js
+++ resources/assets/js/components/Charities/Charity Content/CharityTemplate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 import NavLink from "react-bootstrap/NavLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faGlobe } from "@fortawesome/free-solid-svg-icons";
@@ -22,6 +22,12 @@ const CharityTemplate = props => {
           .nav-link {
             color: white !important;
           }
+          .p {
+            padding: 0px 0px 0px 0px !important;
+            margin: 0px 0px 0px 0px !important;
+            color: white;
+            font-color: white;
+          }
         `}
             </style>
             <Container
@@ -36,23 +42,63 @@ const CharityTemplate = props => {
                 </NavLink>
             </Container>
             {/* BE SURE TO CHECK VW UNITS IN FONT SIZE */}
-
             <Container
                 style={{
                     backgroundColor: "#45A6D7",
                     color: "white",
-                    display: "grid | inline-grid",
-                    gridAutoFlow: "row dense"
+                    display: "flex",
+                    paddingLeft: "0",
+                    paddingRight: "0"
                 }}
             >
-                <div style={{display: 'grid'}}>
-                    <img
-                        src="/img/charity/Victim-Services.png"
-                        style={{ width: "100px" }}
-                    />
-                </div>
+                <Col md="auto" style={{ paddingLeft: "0", paddingRight: "0" }}>
+                    <div>
+                        <img
+                            src="/img/charity/Victim-Services.png"
+                            style={{ width: "100%", height: "100%" }}
+                        />
+                    </div>
+                </Col>
 
-                <div style={{display: 'grid'}}>Placeholder text</div>
+                <Col xs={7} style={{ paddingLeft: "0", paddingRight: "0" }}>
+                    <div style={{ textAlign: "left" }}>
+                        <p
+                            style={{
+                                fontWeight: "700",
+                                fontSize: "1.6vh",
+                                color: "white",
+                                padding: "0px 0px 0px 0px",
+                                margin: "0px 0px 0px 0px"
+                            }}
+                        >
+                            VICTIM SERVICES WELLINGTON (VSW)
+                        </p>
+                        <p
+                            style={{
+                                fontWeight: "300",
+                                fontSize: "1.6vh",
+                                color: "white",
+                                padding: "0px 0px 0px 0px",
+                                margin: "0px 0px 0px 0px"
+                            }}
+                        >
+                            COMMUNITY. CONNECTION. CRISIS SUPPORT.
+                        </p>
+                        <p
+                            style={{
+                                fontWeight: "700",
+                                fontSize: "1.1vh",
+                                color: "white",
+                                padding: "0px 0px 0px 0px",
+                                margin: "0px 0px 0px 0px"
+                            }}
+                        >
+                            Registered Business Name: Victim Services Wellington
+                            <br />
+                            Business Number: 891472763RR0001
+                        </p>
+                    </div>
+                </Col>
             </Container>
 
             {/* SOCIAL MEDIA BUTTON CONTAINER */}
