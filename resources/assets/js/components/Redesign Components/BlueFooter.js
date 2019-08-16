96f8f6e7e41f10933f8588979887e7966a46bec8
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-16 10:03:15 -0400

    Refactoring Components

--- resources/assets/js/components/Redesign Components/BlueFooter.js
+++ resources/assets/js/components/Redesign Components/BlueFooter.js
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
 
-function Footer () {
-  return (
-    <div>
-      <Container style={{
-        backgroundColor: '#26607D',
-        width: '100%' }}>
-        <div style={{
-          color: 'white',
-          display: 'flex',
-          justifyContent: 'center',
-          fontSize: '1rem',
-          marginTop: '7%' }}>
-                Powered By &nbsp;
-          <a href='https://www.synergenics.ca' target='_blank'>
-            <img src='/img/logo/Synergenics-Logo-2008-White.png' style={{ width: '60%' }} />
-          </a>
+function Footer() {
+    return (
+        <div>
+            <Container
+                style={{
+                    backgroundColor: "#26607D",
+                    width: "100%"
+                }}
+            >
+                <div
+                    style={{
+                        color: "white",
+                        display: "flex",
+                        justifyContent: "center",
+                        fontSize: "1rem",
+                        marginTop: "7%"
+                    }}
+                >
+                    Powered By &nbsp;
+                    <a href="https://www.synergenics.ca" target="_blank">
+                        <img
+                            src="/img/logo/Synergenics-Logo-2008-White.png"
+                            style={{ width: "60%" }}
+                        />
+                    </a>
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-
-  )
+    );
 }
 
-export default Footer
+export default Footer;
--- resources/assets/js/components/Text.js
+++ resources/assets/js/components/Text.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 // The Following returns some basic text as a SFC to Main
 
-const Text = (props) => {
-  return  (
-    <div>
-      <h1 style={{marginBottom: 0, marginTop: 20,
-        fontSize: 19}}>{props.text1}</h1>
-      <h2 style={{fontSize: 19, marginBottom: 20}}>{props.text2}</h2>
-    </div>
-  )
-}
+const Text = props => {
+    return (
+        <div>
+            <h1 style={{ marginBottom: 0, marginTop: 20, fontSize: 19 }}>
+                {props.text1}
+            </h1>
+            <h2 style={{ fontSize: 19, marginBottom: 20 }}>{props.text2}</h2>
+        </div>
+    );
+};
 
-export default Text
+export default Text;
