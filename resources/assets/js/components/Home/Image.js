5b4945096efdba66879bb499e4ac940447030cbe
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-19 10:04:50 -0400

    Fix: File Formatting

--- resources/assets/js/components/Home/Image.js
+++ resources/assets/js/components/Home/Image.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Image () {
-  /* Returns the large square logo */
-  return <img src='img/donateABLE-square-colour-500.png' className='img-fluid' alt='Logo' style={{width: 200}} />
+function Image() {
+    /* Returns the large square logo */
+    return (
+        <img
+            src="img/donateABLE-square-colour-500.png"
+            className="img-fluid"
+            alt="Logo"
+            style={{ width: 200 }}
+        />
+    );
 }
 
-export default Image
+export default Image;
--- resources/assets/js/components/Navigation/Modals/Contact/JoinProgram.js
+++ resources/assets/js/components/Navigation/Modals/Contact/JoinProgram.js
@@ -1,47 +1,52 @@
-import React from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import React from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
 
-const JoinProgram = (props) => {
-  return (
+const JoinProgram = props => {
+    return (
+        <Form style={{ marginTop: "5%" }}>
+            <Form.Group as={Row} controlId="formFirstName">
+                <Col sm={10}>
+                    <Form.Control type="text" placeholder="First Name" />
+                </Col>
+            </Form.Group>
 
-    <Form style={{marginTop: '5%'}}>
-      <Form.Group as={Row} controlId='formFirstName'>
-        <Col sm={10}>
-          <Form.Control type='text' placeholder='First Name' />
-        </Col>
-      </Form.Group>
+            <Form.Group as={Row} controlId="formLastName">
+                <Col sm={10}>
+                    <Form.Control type="text" placeholder="Last Name" />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row} controlId='formLastName'>
-        <Col sm={10}>
-          <Form.Control type='text' placeholder='Last Name' />
-        </Col>
-      </Form.Group>
+            <Form.Group as={Row} controlId="formHorizontalEmail">
+                <Col sm={10}>
+                    <Form.Control type="email" placeholder="Email" />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row} controlId='formHorizontalEmail'>
-        <Col sm={10}>
-          <Form.Control type='email' placeholder='Email' />
-        </Col>
-      </Form.Group>
+            <Form.Group as={Row} controlId="formCharityName">
+                <Col sm={10}>
+                    <Form.Control type="text" placeholder="Charity Name" />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row} controlId='formCharityName'>
-        <Col sm={10}>
-          <Form.Control type='text' placeholder='Charity Name' />
-        </Col>
-      </Form.Group> 
+            <Form.Group as={Row} controlId="formAdditionalInfo">
+                <Col sm={10}>
+                    <Form.Control
+                        style={{ paddingBottom: "15%", paddingTop: "5%" }}
+                        type="text"
+                        placeholder="Additional Information"
+                    />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row} controlId='formAdditionalInfo'>
-        <Col sm={10}>
-          <Form.Control style={{paddingBottom: '15%', paddingTop: '5%'}} type='text' placeholder='Additional Information' />
-        </Col>
-      </Form.Group>
+            <Form.Group as={Row}>
+                <Col sm={{ span: 10, offset: 2 }}>
+                    <Button variant={props.variant} type="submit">
+                        Submit
+                    </Button>
+                </Col>
+            </Form.Group>
+        </Form>
+    );
+};
 
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button variant={props.variant} type="submit">Submit</Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  )
-}
-
-export default JoinProgram 
+export default JoinProgram;
