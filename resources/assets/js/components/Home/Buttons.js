5d539d259abc209d22cead6781522ef6e2b82bdc
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-23 15:58:39 -0400

    Fix: Decoupled modals used on home page

--- resources/assets/js/components/Home/Buttons.js
+++ resources/assets/js/components/Home/Buttons.js
@@ -1,85 +1,134 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import TourModal from "../Navigation/Modals/Tour/TourModal";
+import HowItWorksModal from "../Navigation/Modals/HowItWorks/HowItWorksModal";
 
-function Buttons() {
-    return (
-        /* Custom styling for buttons, sizing and colors */
-        <div className="text-center">
-            <style className="text/css">
-                {`
-                .btn-turq {
-                  background-color: #45A6D7;
-                  border-color: #979797;
-                  border-radius: 0;
-                  color: white;
-                  vertical-align: middle;
-                  font-weight: 500;
-                  letter-spacing: 0.05em;
-                  font-family: 'Montserrat-Bold', sans-serif;
-                  margin-bottom: 6px;
-                  width: 305px;
-                  height: 48px;
-                  font-size: 10pt;
-                }
-      
-                .btn-turqdark{
-                  background-color: #26607D;
-                  border-color: #979797;
-                  border-radius: 0;
-                  color: white;
-                  vertical-align: middle;
-                  font-weight: 500;
-                  letter-spacing: 0.05em;
-                  font-family: 'Montserrat-Bold', sans-serif;
-                  margin-bottom: 5px;
-                  width: 305px;
-                  height: 48px;
-                  font-size: 10pt;
-                }
-      
-                .btn-turqwhite{
-                  background-color: #FFFFFF;
-                  border-color: #979797;
-                  border-radius: 0;
-                  color: #26607D;
-                  font-weight: 500;
-                  letter-spacing: 0.05em;
-                  font-family: 'Montserrat-Bold', sans-serif;
-                  margin-bottom: 5px;
-                  width: 305px;
-                  height: 48px;
-                  font-size: 10pt;
-                }
-        `}
-            </style>
-            <NavLink to="/charities" style={{ textDecoration: "none" }}>
-                <Button variant="turqdark" className="row" size="lg">
-                    SELECT YOUR CHARITY
-                </Button>
-            </NavLink>
+class Buttons extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            showHIW: false,
+            showTour: false
+        };
+    }
 
-            <Button variant="turqwhite" size="lg">
-                HOW IT WORKS
-            </Button>
-            <Button variant="turqwhite" size="lg">
-                TAKE A TOUR
-            </Button>
-            <Button variant="turq" size="lg">
-                CURRENTLY DONATING{" "}
-            </Button>
-            <NavLink to="/wvs" style={{ textDecoration: "none" }}>
-                <Button variant="turq" size="lg">
-                    DONATORS TO DATE
+    /* State Handling Functions */
+
+    // How it Works
+    handleShowHIW = () => {
+        this.setState({ showHIW: true });
+    };
+
+    handleHideHIW = () => {
+        this.setState({ showHIW: false });
+    };
+
+    // Take a Tour
+    handleShowTour = () => {
+        this.setState({ showTour: true });
+    };
+
+    handleHideTour = () => {
+        this.setState({ showTour: false });
+    };
+
+    /* Render Method for this Component */
+    render() {
+        return (
+            /* Custom styling for buttons, sizing and colors */
+            <div className="text-center">
+                <style className="text/css">
+                    {`
+                    .btn-turq {
+                      background-color: #45A6D7;
+                      border-color: #979797;
+                      border-radius: 0;
+                      color: white;
+                      vertical-align: middle;
+                      font-weight: 500;
+                      letter-spacing: 0.05em;
+                      font-family: 'Montserrat-Bold', sans-serif;
+                      margin-bottom: 6px;
+                      width: 305px;
+                      height: 48px;
+                      font-size: 10pt;
+                    }
+          
+                    .btn-turqdark{
+                      background-color: #26607D;
+                      border-color: #979797;
+                      border-radius: 0;
+                      color: white;
+                      vertical-align: middle;
+                      font-weight: 500;
+                      letter-spacing: 0.05em;
+                      font-family: 'Montserrat-Bold', sans-serif;
+                      margin-bottom: 5px;
+                      width: 305px;
+                      height: 48px;
+                      font-size: 10pt;
+                    }
+          
+                    .btn-turqwhite{
+                      background-color: #FFFFFF;
+                      border-color: #979797;
+                      border-radius: 0;
+                      color: #26607D;
+                      font-weight: 500;
+                      letter-spacing: 0.05em;
+                      font-family: 'Montserrat-Bold', sans-serif;
+                      margin-bottom: 5px;
+                      width: 305px;
+                      height: 48px;
+                      font-size: 10pt;
+                    }
+            `}
+                </style>
+                <NavLink to="/charities" style={{ textDecoration: "none" }}>
+                    <Button variant="turqdark" className="row" size="lg">
+                        SELECT YOUR CHARITY
+                    </Button>
+                </NavLink>
+                <Button
+                    variant="turqwhite"
+                    size="lg"
+                    onClick={this.handleShowHIW}
+                >
+                    HOW IT WORKS
                 </Button>
-            </NavLink>
-            <NavLink to="/account" style={{ textDecoration: "none" }}>
-                <Button variant="turq" size="lg">
-                    TOTAL HASHES 193 832
+                <Button
+                    variant="turqwhite"
+                    size="lg"
+                    onClick={this.handleShowTour}
+                >
+                    TAKE A TOUR
                 </Button>
-            </NavLink>
-        </div>
-    );
+                <Button variant="turq" size="lg">
+                    CURRENTLY DONATING
+                </Button>
+                <NavLink to="/wvs" style={{ textDecoration: "none" }}>
+                    <Button variant="turq" size="lg">
+                        DONATORS TO DATE
+                    </Button>
+                </NavLink>
+                <NavLink to="/account" style={{ textDecoration: "none" }}>
+                    <Button variant="turq" size="lg">
+                        TOTAL HASHES 193 832
+                    </Button>
+                </NavLink>
+
+                <HowItWorksModal
+                    show={this.state.showHIW}
+                    hide={this.handleHideHIW}
+                />
+                <TourModal
+                    show={this.state.showTour}
+                    hide={this.handleHideTour}
+                />
+            </div>
+        );
+    }
 }
 
 export default Buttons;
