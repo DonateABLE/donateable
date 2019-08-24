22be61afb6ee33428b2b0d4dad6cc82a721e31e0
Luke Pritchard <luke.pritchard@protonmail.com>
2019-08-23 21:23:21 -0400

    Refactoring Old Components

--- resources/assets/js/components/Login/LoginForm.js
+++ resources/assets/js/components/Login/LoginForm.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import React from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
 
-const LoginForm = ( props ) => {
-  return (
-    <Form>
-      <Form.Group as={Row} controlId='loginEmail'>
-        <Col sm={10}>
-          <Form.Control type='email' placeholder='Email' />
-        </Col>
-      </Form.Group>
+const LoginForm = props => {
+    return (
+        <Form>
+            <Form.Group as={Row} controlId="loginEmail">
+                <Col sm={10}>
+                    <Form.Control type="email" placeholder="Email" />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row} controlId='loginPassword'>
-        <Col sm={10}>
-          <Form.Control type='password' placeholder='Password' />
-        </Col>
-      </Form.Group>
+            <Form.Group as={Row} controlId="loginPassword">
+                <Col sm={10}>
+                    <Form.Control type="password" placeholder="Password" />
+                </Col>
+            </Form.Group>
 
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button variant={props.variant} type="submit">Login</Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  )
-}
-export default LoginForm
+            <Form.Group as={Row}>
+                <Col sm={{ span: 10, offset: 2 }}>
+                    <Button variant={props.variant} type="submit">
+                        Login
+                    </Button>
+                </Col>
+            </Form.Group>
+        </Form>
+    );
+};
+export default LoginForm;
