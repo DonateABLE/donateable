8c64b691ba69b41e09c7173084d6b5d4cf33bd2c
Luke Pritchard <luke.pritchard@protonmail.com>
2019-09-12 16:23:28 -0400

    Fix: Charity Tabs with new Stats.js Component

--- resources/assets/js/components/Charities/Content/TabbedContainer.js
+++ resources/assets/js/components/Charities/Content/TabbedContainer.js
@@ -12,6 +12,7 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import ProgressBar from "./ProgressBar";
 import TwoColorHeader from "./TwoColorHeader";
 import DonateSlider from "./DonateSlider";
+import Stats from "./Stats";
 
 // Tabbed Content Container
 const TabbedContainer = props => {
@@ -182,59 +183,7 @@ const TabbedContainer = props => {
                                     lightText="STATISTICS"
                                 />
                             </div>
-
-                            <img
-                                src="/img/charity/Victim-Services.png"
-                                style={{ width: "50%", margin: "auto" }}
-                            />
-                            <h2> VICTIM SERVICES WELLINGTON </h2>
-                            <h2 style={{ fontWeight: "400", fontSize: "16px" }}>
-                                {" "}
-                                STATISTICS{" "}
-                            </h2>
-
-                            <ProgressBar
-                                label="2"
-                                percentage={22}
-                                title="CURRENTLY DONATING"
-                            />
-                            <ProgressBar
-                                label="34"
-                                percentage={56}
-                                title="DONATORS TO DATE"
-                            />
-                            <ProgressBar
-                                label="62M"
-                                percentage={70}
-                                title="TOTAL HASHES"
-                                details="62 406 532"
-                            />
-
-                            <LineBreak />
-
-                            <img
-                                src="/img/logo/D-Coloured-250x250.png"
-                                style={{ width: "25%", margin: "auto" }}
-                            />
-                            <h2> DONATEABLE </h2>
-                            <h2> STATISTICS </h2>
-
-                            <ProgressBar
-                                label="15"
-                                percentage={34}
-                                title="CURRENTLY DONATING"
-                            />
-                            <ProgressBar
-                                label="66"
-                                percentage={363}
-                                title="DONATORS TO DATE"
-                            />
-                            <ProgressBar
-                                label="3B"
-                                percentage={82}
-                                title="TOTAL HASHES"
-                                details="3 916 406 532"
-                            />
+                            <Stats />
                         </Tab>
 
                         {/* DONATION TARGETS TAB */}
@@ -281,7 +230,6 @@ const TabbedContainer = props => {
                                 darkText="DONATE"
                                 lightText="NOW"
                             />
-                            {/* THESE BARS WILL NEED TO BE ANIMATED AT ONE POINT */}
                             <ProgressBar
                                 label="0"
                                 percentage={0}
