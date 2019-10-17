331b86d84ddb0ddf26783d6f126a5b6f5dccc0b0
Luke Pritchard <luke.pritchard@protonmail.com>
2019-10-17 13:53:10 -0400

    Fix: Home page revision updates

--- resources/assets/js/components/Footer.js
+++ resources/assets/js/components/Footer.js
@@ -3,27 +3,25 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
     return (
-        <div id="footer" style={{paddingTop: "10%"}}>
+        <div id="footer">
             <Container
                 style={{
-                    backgroundColor: "#26607D",
-                    color: "white",
+                    backgroundColor: "white",
+                    color: "#26607D",
                     fontSize: "0.95rem",
                     fontWeight: "500",
                     width: "100%",
                     height: "70px"
                 }}
             >
-                <Row style={{paddingTop: "3%"}}>
-                    <Col className="text-center">
-                        Powered By &nbsp;
-                    </Col>
+                <Row>
+                    <Col className="text-center">Powered By &nbsp;</Col>
                 </Row>
                 <Row>
                     <Col className="text-center">
                         <a href="https://www.synergenics.ca" target="_blank">
                             <img
-                                src="/img/logo/Synergenics-Logo-2008-White.png"
+                                src="/img/logo/Synergenics-Logo.png"
                                 style={{ width: "60%", maxWidth: "134px" }}
                             />
                         </a>
